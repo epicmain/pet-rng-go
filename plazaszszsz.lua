2ba8051b3ca54dcec9674e8099974f1bb4fe548f
epicmain <85509340+epicmain@users.noreply.github.com>
2025-02-01 17:02:32 +0800

    Update plazaszszsz.lua

--- plazaszszsz.lua
+++ plazaszszsz.lua
@@ -14,13 +14,8 @@ getgenv().petsGoConfig = {
         "ohiosigmawth",
         "ohiosigmawth1",
         "ohiosigmawth2",
-        "ohiosigmawth3",
-        "ohiosigmawth4",
-        "ohiosigmawth5",
         "ohiosigmawth6",
-        "ohiosigmawth7",
         "ohiosigmawth8",
-        "ohiosigmawth9",
     },  -- serverhop when it finds username, You can put multiple usernames
     
     -- Pets
@@ -60,7 +55,7 @@ getgenv().petsGoConfig = {
     SELL_CHARGE_TOKEN = true, SELL_CHARGE_TOKEN_MIN = 5, SELL_CHARGE_TOKEN_PERCENTAGE = 0.9,
 
     MAIL_GEMS_USERNAME = {""},  -- Empty = Disabled Gem Mail, You can put multiple usernames 1% fee
-    MAIL_GEMS_MIN = 10000000,  -- Minimum gems required to send
+    MAIL_GEMS_MIN = 100000000,  -- Minimum gems required to send
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/074ae419ea26f2a773d8e592958ec2e8.lua"))() 
